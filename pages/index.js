dda3b15666cbf5032ca82ec3660e322189e8fd5a
Timothy Ko <timothy.l.ko@gmail.com>
2020-04-08 15:07:13 -0700

    fix cali

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,6 @@ const Home = () => {
   const { today: ilToday, hist: ilHist, todayHist: todayILHist, info: ilInfo } = fetchData("IL")
   const { today: nyToday, hist: nyHist, todayHist: todayNYHist, info: nyInfo } = fetchData("NY")
 
-  const betterCaliHist = caliHist ? getHospitalizedIncr(caliHist) : {}
   const hospitalizedData = caliHist ? getHospitalizedIncr(caliHist).map(day => ({ date: prettyDate(day.dateChecked, true), hospitalized: day.hospitalizedCurrently, new: day.hospitalizedIncrease })).reverse() : []
   const deathData = caliHist ? caliHist.map(day => ({ date: prettyDate(day.dateChecked, true), deaths: day.death, new: day.deathIncrease })).reverse() : []
   const caliConfirmedData = caliHist ? caliHist.map(day => ({ date: prettyDate(day.dateChecked, true), confirmed: day.positive, new: day.positiveIncrease })).reverse() : []
@@ -101,7 +100,7 @@ const Home = () => {
         </div>
         <div className='row'>
           <div className='grid'>
-            <StateStatCard state='California' today={caliToday} hist={betterCaliHist} population={population.states.california} stateInfo={caliInfo} />
+            <StateStatCard state='California' today={caliToday} hist={todayCaliHist} population={population.states.california} stateInfo={caliInfo} />
             <StateStatCard state='Illinois' today={ilToday} hist={todayILHist} population={population.states.illinois} stateInfo={ilInfo} />
             <StateStatCard state='New York' today={nyToday} hist={todayNYHist} population={population.states.new_york} stateInfo={nyInfo} />
             <HistTable state='California' data={caliHist} population={population.states.california} stateInfo={caliInfo} />
